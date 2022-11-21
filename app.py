da192a0b47da086b8e7753ea6c3566cdeafc1e82
Seyi-Banjo <elijahbanjo@gmail.com>
2022-11-21 22:35:46 +0100

    Added Comments

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 import random
 
+#names of people to be paired
 names = ["Frank","Shola","Tolu","Timmy","David","Daniel","John","Matthew","James","Paul", "Jude", "Ralf"]
 
+#Pairing list
 pairing = []
+
+#Looping through the pairing len/2 times because pair is in 2
 for i in range(int(len(names)/2)):
+    #Generate first random name
     brother1 = random.choice(names)
+    #Remove generated name from list
     names.remove(brother1)
+    #Generate second name
     brother2 = random.choice(names)
+    #Remove generated name from list
     names.remove(brother2)
+    #Join generated pair and add it to list array
     pairing.append("Bro "+brother1 + " & " + "Bro "+brother2)
 
 print("The Pairings Are")
 
+#Print pairing
 for i in pairing:
     print(i)
