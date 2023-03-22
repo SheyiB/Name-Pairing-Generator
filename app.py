06702d709a206a0f73541b5a155896a71fec1c99
Seyi-Banjo <elijahbanjo@gmail.com>
2023-03-22 22:34:25 +0100

    Created Generate Pair function

--- app.py
+++ app.py
@@ -6,18 +6,19 @@ names = ["Frank","Shola","Tolu","Timmy","David","Daniel","John","Matthew","James
 #Pairing list
 pairing = []
 
-#Looping through the pairing len/2 times because pair is in 2
-for i in range(int(len(names)/2)):
-    #Generate first random name
-    brother1 = random.choice(names)
-    #Remove generated name from list
-    names.remove(brother1)
-    #Generate second name
-    brother2 = random.choice(names)
-    #Remove generated name from list
-    names.remove(brother2)
-    #Join generated pair and add it to list array
-    pairing.append("Bro "+brother1 + " & " + "Bro "+brother2)
+def generatePairs():
+    #Looping through the pairing len/2 times because pair is in 2
+    for i in range(int(len(names)/2)):
+        #Generate first random name
+        brother1 = random.choice(names)
+        #Remove generated name from list
+        names.remove(brother1)
+        #Generate second name
+        brother2 = random.choice(names)
+        #Remove generated name from list
+        names.remove(brother2)
+        #Join generated pair and add it to list array
+        pairing.append("Bro "+brother1 + " & " + "Bro "+brother2)
 
 print("The Pairings Are")
 
