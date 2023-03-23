7e64b99f64ce6f125df2b56d3608535c47715e0b
Seyi-Banjo <elijahbanjo@gmail.com>
2023-03-23 23:21:27 +0100

    CleanUp

--- app.py
+++ app.py
@@ -32,32 +32,33 @@ class NamesPairGen:
             for y in self.name_pairs[x]:
                 print(y, end= ' ')
             print(" ")
-def generatePairs(names, pairLength):
-    #Looping through the pairing len/2 times because pair is in 2
-    for i in range(int(len(names)/pairLength)):
-        currentPair = []
-        for x in range(i):
-            currentPair.append(random.choice(names))
-        #Generate first random name
-        brother1 = random.choice(names)
-        #Remove generated name from list
-        names.remove(brother1)
-        #Generate second name
-        brother2 = random.choice(names)
-        #Remove generated name from list
-        names.remove(brother2)
-        #Join generated pair and add it to list array
-        pairing.append("Bro "+brother1 + " & " + "Bro "+brother2)
 
-def showPairs():
-    print("The Pairings Are")
-    #Print pairing
-    for i in pairing:
-        print(i)
+# def generatePairs(names, pairLength):
+#     #Looping through the pairing len/2 times because pair is in 2
+#     for i in range(int(len(names)/pairLength)):
+#         currentPair = []
+#         for x in range(i):
+#             currentPair.append(random.choice(names))
+#         #Generate first random name
+#         brother1 = random.choice(names)
+#         #Remove generated name from list
+#         names.remove(brother1)
+#         #Generate second name
+#         brother2 = random.choice(names)
+#         #Remove generated name from list
+#         names.remove(brother2)
+#         #Join generated pair and add it to list array
+#         pairing.append("Bro "+brother1 + " & " + "Bro "+brother2)
+
+# def showPairs():
+#     print("The Pairings Are")
+#     #Print pairing
+#     for i in pairing:
+#         print(i)
 
 
 churchBrothers = NamesPairGen(names)
 
-churchBrothers.generate_pairs(2)
+churchBrothers.generate_pairs(4)
 
 churchBrothers.show_pairs()
