8ac6c9369672a62a05498e6f3dc0f3bd706ddc95
Seyi-Banjo <elijahbanjo@gmail.com>
2023-03-23 23:21:52 +0100

    Update

--- app.py
+++ app.py
@@ -6,7 +6,28 @@ names = ["Frank","Shola","Tolu","Timmy","David","Daniel","John","Matthew","James
 #Pairing list
 pairing = []
 
+# def generatePairs(names, pairLength):
+#     #Looping through the pairing len/2 times because pair is in 2
+#     for i in range(int(len(names)/pairLength)):
+#         currentPair = []
+#         for x in range(i):
+#             currentPair.append(random.choice(names))
+#         #Generate first random name
+#         brother1 = random.choice(names)
+#         #Remove generated name from list
+#         names.remove(brother1)
+#         #Generate second name
+#         brother2 = random.choice(names)
+#         #Remove generated name from list
+#         names.remove(brother2)
+#         #Join generated pair and add it to list array
+#         pairing.append("Bro "+brother1 + " & " + "Bro "+brother2)
 
+# def showPairs():
+#     print("The Pairings Are")
+#     #Print pairing
+#     for i in pairing:
+#         print(i)
 
 class NamesPairGen:
     def __init__(self, name_list = []):
@@ -32,30 +53,6 @@ class NamesPairGen:
             for y in self.name_pairs[x]:
                 print(y, end= ' ')
             print(" ")
-
-# def generatePairs(names, pairLength):
-#     #Looping through the pairing len/2 times because pair is in 2
-#     for i in range(int(len(names)/pairLength)):
-#         currentPair = []
-#         for x in range(i):
-#             currentPair.append(random.choice(names))
-#         #Generate first random name
-#         brother1 = random.choice(names)
-#         #Remove generated name from list
-#         names.remove(brother1)
-#         #Generate second name
-#         brother2 = random.choice(names)
-#         #Remove generated name from list
-#         names.remove(brother2)
-#         #Join generated pair and add it to list array
-#         pairing.append("Bro "+brother1 + " & " + "Bro "+brother2)
-
-# def showPairs():
-#     print("The Pairings Are")
-#     #Print pairing
-#     for i in pairing:
-#         print(i)
-
 
 churchBrothers = NamesPairGen(names)
 
