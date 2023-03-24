2343dbcda47e632868a06021e77be94cc02ff790
Seyi-Banjo <elijahbanjo@gmail.com>
2023-03-24 23:34:32 +0100

    CleanUp

--- testt.py
+++
@@ -1,9 +0,0 @@
-def area_of_rectangle(length, breadth):
-    return length * breadth
-
-def area_of_square():
-    length = int(input("What is the length"))
-    area = length**2
-    print("The area of the square", area)
-
-print("The Area is", area_of_rectangle(10,20))
