1e8a06ecbe9525a6f19c0d61c306090a427a1290
Seyi-Banjo <elijahbanjo@gmail.com>
2023-03-24 23:32:43 +0100

    CleanUp

--- app.py
+++ app.py
@@ -11,31 +11,6 @@ with open(sys.argv[1], newline='') as csvfile:
         bch_students.append(row)
 names = ["Johny","Frank","Shola","Tolu","Timmy","David","Daniel","John","Matthew","James","Paul", "Jude", "Ralf"]
 
-#Pairing list
-pairing = []
-
-# def generatePairs(names, pairLength):
-#     #Looping through the pairing len/2 times because pair is in 2
-#     for i in range(int(len(names)/pairLength)):
-#         currentPair = []
-#         for x in range(i):
-#             currentPair.append(random.choice(names))
-#         #Generate first random name
-#         brother1 = random.choice(names)
-#         #Remove generated name from list
-#         names.remove(brother1)
-#         #Generate second name
-#         brother2 = random.choice(names)
-#         #Remove generated name from list
-#         names.remove(brother2)
-#         #Join generated pair and add it to list array
-#         pairing.append("Bro "+brother1 + " & " + "Bro "+brother2)
-
-# def showPairs():
-#     print("The Pairings Are")
-#     #Print pairing
-#     for i in pairing:
-#         print(i)
 
 class NamesPairGen:
     def __init__(self, name_list = []):
@@ -68,15 +43,17 @@ class NamesPairGen:
         for x in range(len(self.name_pairs)):
             #print("Group ", x+1)
             with open('result.csv', 'a', newline='') as csvfile:
-                result = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                result = csv.writer(csvfile, delimiter=' ', quoting=csv.QUOTE_MINIMAL)
                 result.writerow(['Group', x+1])
             for y in self.name_pairs[x]:
                # print(y, end= ' ')
                 with open('result.csv', 'a', newline='') as csvfile:
-                    result = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+                    result = csv.writer(csvfile, delimiter=' ',  quoting=csv.QUOTE_MINIMAL)
                     result.writerow(y)
+            
+            with open('result.csv', 'a', newline='') as csvfile:
+                    result = csv.writer(csvfile, delimiter=' ', quoting=csv.QUOTE_MINIMAL)
                     result.writerow('\n')
-
             #print(" ")
 
 churchBrothers = NamesPairGen(bch_students)
